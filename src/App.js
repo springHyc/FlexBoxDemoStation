d7240bd0dfd745c7e06df4199cd1f9d7cbaf788b
贺贺 <zhulinger520@163.com>
2018-05-07 16:30:00 +0800

    chore: 添加文本说明

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">FlexBox 演示站</h1>
+          <span>
+            如果你觉得这个FlexBox 演示站对你有所帮助，请点击<a href="https://github.com/springHyc/FlexBoxDemoStation">
+              链接
+            </a>，给我个star，非常感谢！
+          </span>
         </header>
         <FlexBoxContainer />
       </div>
